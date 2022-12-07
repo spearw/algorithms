0f372dc5bf63eb6f20250bd6f2ced95f5c9b34b2
spearw <spearw@gmail.com>
2022-12-07 11:15:09 -0500

    Updated tsp to sort subsets by path length instead of by cities

--- tsp/euclidean_tsp.py
+++ tsp/euclidean_tsp.py
@@ -1,9 +1,12 @@
+import time
+
 from math import dist
 
 import itertools
 
 
 def tsp(graph, precision=2):
+    start_time = time.time()
     num_cities = len(graph)
     subsets = gen_subsets(num_cities)
     final_path = float('inf')
@@ -13,44 +16,51 @@ def tsp(graph, precision=2):
     for i in range(0, num_cities):
         paths[0].append([graph[0][i], 0])
 
+    print("Base cases completed")
+
     # Iterate over increasing path lengths
     for i in range(1, num_cities - 1):
 
         # Add new path length to paths
         paths.append({})
 
-        # Calculate paths
-        for city_set in subsets:
-            destination_city = city_set[0]
-            for path in city_set:
-                if not type(path) == int:
-                    if len(path) == i:
+        # Calculate paths for each increasing length, starting with length 1
+        for path_set in subsets:
+            # Skip placeholder set
+            if len(path_set) == 0:
+                continue
+            for route in path_set:
+                destination_city = route[0]
+                path = route[1]
 
-                        # take shortest path of all possible ways
-                        items = list(map(str, path))
-                        permutations_list = list(itertools.permutations(items, len(items)))
-                        shortest_path = float('inf')
-                        for permutation in permutations_list:
-                            try:
-                                new_path_length = paths[i-1][int(''.join(map(str, permutation)))][0]
-                                end_hop_length = graph[destination_city][int(permutation[0])]
-                                shortest_path = min(shortest_path, new_path_length + end_hop_length)
-                            except:
-                                continue
+                # take shortest path of all possible ways
+                items = list(map(str, path))
+                permutations_list = list(itertools.permutations(items, len(items)))
+                shortest_path = float('inf')
+                for permutation in permutations_list:
+                    try:
+                        new_path_length = paths[i-1][int(''.join(map(str, permutation)))][0]
+                        end_hop_length = graph[destination_city][int(permutation[0])]
+                        shortest_path = min(shortest_path, new_path_length + end_hop_length)
+                    except:
+                        continue
 
-                        paths[i][int(f"{destination_city}{''.join(map(str, path))}")] = [shortest_path, city_set[0]]
-                    elif len(path) > i:
-                        # Move on to next city_set
-                        pass
+                paths[i][int(f"{destination_city}{''.join(map(str, path))}")] = [shortest_path, path_set[0]]
+
+        print(f"Paths of length {i} calculations completed")
 
     # Calculate final hop
     for path in paths[-1]:
         final_path = min(final_path, paths[0][int(str(path)[0])][0] + paths[-1][path][0])
 
+    print("--- %s seconds ---" % (time.time() - start_time))
     return round(final_path, precision)
 
 def gen_subsets(num_cities):
+    #TODO: gen better. Should be sorted by len, so can just grab the appropriate parts. Sort combinations as they come out into final buckets based on size
     all_combinations = []
+    for i in range(num_cities):
+        all_combinations.append([])
     # 0 is assumed starter city, thus removed from sets for being special case
     for city in range(1, num_cities):
         combinations = [city]
@@ -58,8 +68,7 @@ def gen_subsets(num_cities):
         city_set.remove(city)
         for i in range(1, num_cities):
             for combination in itertools.combinations(city_set, i):
-                combinations.append(combination)
-        all_combinations.append(combinations)
+                all_combinations[len(combination)].append([city, combination])
     return all_combinations
 
 def calc_euclidean_distances(path):
@@ -68,7 +77,9 @@ def calc_euclidean_distances(path):
         lines = file.read().splitlines()
 
     num_cities = int(lines.pop(0))
+    print(f"Number of cities: {num_cities}")
     answer = lines.pop()
+    print(f"Answer: {answer}")
     all_distances = []
     # Calculate distance between every city, and store in 2-d list
     for line1 in lines:
@@ -79,6 +90,7 @@ def calc_euclidean_distances(path):
             distances.append(dist(cord1, cord2))
         all_distances.append(distances)
 
+    print(f"distances: {all_distances}")
     return all_distances
 
 
