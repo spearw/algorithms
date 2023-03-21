9ce15b3970b8f956bb2b558c44f78116e9b69bb8
spearw <spearw@gmail.com>
2023-03-20 21:33:17 -0400

    Working and test cases

--- tsp/python/test_tsp_heuristic.py
+++ tsp/python/test_tsp_heuristic.py
@@ -4,7 +4,10 @@ from tsp.python.tsp_heuristic import tsp_heuristic as tsp
 class TestStringMethods(unittest.TestCase):
 
     def test_tsp(self):
-        self.assertEqual(tsp("dat/tsp_heuristic/t1.txt"), [1, 3, 2, 5, 6, 4, 1])
+        self.assertEqual(tsp("dat/tsp_heuristic/t1.txt"), 15)
+        self.assertEqual(tsp("dat/tsp_heuristic/t2.txt"), 2470)
+        self.assertEqual(tsp("dat/tsp_heuristic/t3.txt"), 48581)
+        self.assertEqual(tsp("dat/tsp_heuristic/t4.txt"), 188129)
 
 if __name__ == '__main__':
     unittest.main()
--- tsp/python/tsp_heuristic.py
+++ tsp/python/tsp_heuristic.py
@@ -1,4 +1,4 @@
-from math import dist
+import math
 
 def tsp_heuristic(path):
     with open(path) as file:
@@ -21,36 +21,18 @@ def tsp_heuristic(path):
     current_city = find_closest_city(current_city[1], [first_city[1]])
     path.append(int(current_city[1].split(' ')[0]))
     pathlength += current_city[0]
-    return path, pathlength
+
+    print(path)
+    return math.floor(pathlength)
 
 def find_closest_city(current_city, remaining_cities):
     distances = {}
     cord1 = tuple(map(float, current_city.split(' ')))
     for city in remaining_cities:
         cord2 = tuple(map(float, city.split(' ')))
-        distances[cord2[0]] = [dist(cord1[1:], cord2[1:]), city]
+        distances[cord2[0]] = [math.dist(cord1[1:], cord2[1:]), city]
     next_city = min(distances, key=distances.get)
     return distances[next_city]
-
-
-def calc_euclidean_distances(path):
-
-    with open(path) as file:
-        lines = file.read().splitlines()
-
-    num_cities = int(lines.pop(0))
-    answer = lines.pop()
-    all_distances = []
-    # Calculate distance between every city, and store in 2-d list
-    for line1 in lines:
-        distances = []
-        for line2 in lines:
-            cord1 = tuple(map(float, line1.split(' ')))
-            cord2 = tuple(map(float, line2.split(' ')))
-            distances.append(dist(cord1, cord2))
-        all_distances.append(distances)
-
-    return all_distances
 
 if __name__ == "__main__":
     # print(tsp(calc_euclidean_distances("dat/tsp/tsp.txt")))
